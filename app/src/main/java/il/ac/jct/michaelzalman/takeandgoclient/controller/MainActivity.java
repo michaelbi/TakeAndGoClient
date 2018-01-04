b5fc7c5643918a12a94eb20025fe912cf15520ba
michaelbi <10m.b10@gmail.com>
2018-01-04 23:11:51 +0200

    הוספת נתונים מאפליקציה ראשונה

--- app/src/main/java/il/ac/jct/michaelzalman/takeandgoclient/controller/MainActivity.java
+++ app/src/main/java/il/ac/jct/michaelzalman/takeandgoclient/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package il.ac.jct.michaelzalman.takeandgoclient;
+package il.ac.jct.michaelzalman.takeandgoclient.controller;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import il.ac.jct.michaelzalman.takeandgoclient.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
